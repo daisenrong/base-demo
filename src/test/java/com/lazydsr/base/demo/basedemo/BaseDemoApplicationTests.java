337e900d97190c73793efa02ebf7636e3c683445
daisenrong <daisenrong@outlook.com>
2020-06-02 23:31:42 +0800

    init

---
+++ src/test/java/com/lazydsr/base/demo/basedemo/BaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazydsr.base.demo.basedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
