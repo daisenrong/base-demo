56cecf137990ddb4f592c29fc1b835616bb96556
daisenrong <daisenrong@guazi.com>
2020-06-10 13:37:20 +0800

    init

--- src/main/java/com/lazydsr/base/demo/basedemo/BaseDemoApplication.java
+++ src/main/java/com/lazydsr/base/demo/basedemo/BaseDemoApplication.java
@@ -1,14 +1,20 @@
 package com.lazydsr.base.demo.basedemo;
 
+import com.lazydsr.base.demo.basedemo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @SpringBootApplication
+@Slf4j
 public class BaseDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseDemoApplication.class, args);
     }
-
 }
